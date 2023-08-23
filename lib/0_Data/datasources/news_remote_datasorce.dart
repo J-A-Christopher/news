0c41623228eb2c7eda94f21c92186fa7f165b39c
Cj <christopherjesse45@gmail.com>
2023-08-24 00:33:06 +0300

    Full fledged application done!

--- lib/0_Data/datasources/news_remote_datasorce.dart
+++ lib/0_Data/datasources/news_remote_datasorce.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:news/0_Data/exceptions/exceptions.dart';
+
 import '../models/news_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,8 +14,13 @@ class NewsRemoteDataSourceImpl implements NewsRemoteDataSource {
   Future<News> getNewsFromApi() async {
     final response = await http.get(Uri.parse(
         'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8b38b42439bd40d3a599005d0cc8146d'));
-    var responseBody = json.decode(response.body);
-    print('pizo$responseBody');
-    return News.fromJson(responseBody);
+
+    if (response.statusCode != 200) {
+      throw ServerException();
+    } else {
+      var responseBody = json.decode(response.body);
+      print('pizo$responseBody');
+      return News.fromJson(responseBody);
+    }
   }
 }
--- lib/0_Data/repositories/news_repo_implementation.dart
+++ lib/0_Data/repositories/news_repo_implementation.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:news/0_Data/datasources/news_remote_datasorce.dart';
+import 'package:news/0_Data/exceptions/exceptions.dart';
 import 'package:news/1_Domain/entities/news_entity.dart';
 import 'package:news/1_Domain/failure/failure.dart';
 import 'package:news/1_Domain/repositories/news_repo.dart';
@@ -8,7 +9,13 @@ class NewsRepoImpl implements NewsRepo {
   final NewsRemoteDataSource newsRemoteDataSource = NewsRemoteDataSourceImpl();
   @override
   Future<Either<Failure, NewsEntity>> getnewsData() async {
-    final result = await newsRemoteDataSource.getNewsFromApi();
-    return right(result);
+    try {
+      final result = await newsRemoteDataSource.getNewsFromApi();
+      return right(result);
+    } on ServerException catch (_) {
+      return left(ServerFailure());
+    } catch (error) {
+      return left(GeneralFailure());
+    }
   }
 }
